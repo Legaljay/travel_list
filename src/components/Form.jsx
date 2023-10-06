9ad1b9b008278526506a746cb8035ea128e7b1c3
Legaljay <adesinaayobami67@gmail.com>
2023-10-06 23:44:36 +0100

    hi

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,7 @@ export const Form = () => {
             packed: false, 
             id:Date.now()
         };
-        //     console.log('new 
+        //     console.log('ne
         setDescription('');
         setQuantity(1);
     }
